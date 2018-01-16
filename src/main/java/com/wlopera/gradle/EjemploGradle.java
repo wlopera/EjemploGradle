1bbac73e1d86940308271ff2ecc476eba62adfaf
wlopera <lopera.william@gmail.com>
2018-01-15 22:45:50 -0500

    Inicio ejemplo basico gradle

---
+++ src/main/java/com/wlopera/gradle/EjemploGradle.java
@@ -0,0 +1,15 @@
+package com.wlopera.gradle;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class EjemploGradle {
+
+	private static final Logger logger = LogManager.getLogger(EjemploGradle.class);
+	 
+	public static void main(String[] args) {
+		
+		logger.info("##=> Ejemplo basico de Gradle ...!");
+	}
+
+}
